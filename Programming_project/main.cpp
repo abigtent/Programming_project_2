b4dc0809c26002ce4a0b7f9ef8d625ba04dbd0af
abigtent <100847889+abigtent@users.noreply.github.com>
2022-09-26 14:01:52 +0200

    Initial Commit

--- Programming_project/main.cpp
+++ Programming_project/main.cpp
@@ -6,9 +6,42 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void get_menu();
+int inv_guests();
+
+int main() {
+    get_menu();
+    int guests = inv_guests();
+    
     return 0;
 }
+
+void get_menu() {
+    cout << "1. Enter number of invited guests." << endl;
+    cout << "2. Determine the number of invitation cards and sweets." << endl;
+    cout << "3. Determine the number of tables needed." << endl;
+    cout << "4. Determine drinks order." << endl;
+    cout << "5. Cost of invitation cards." << endl;
+    cout << "6. Cost of drinks" << endl;
+    cout << "7. Display all information." << endl;
+    cout << "8. Quit." << endl;
+}
+
+int inv_guests() {
+    int guests = 0;
+    bool valid_int = false;
+    while (valid_int != true) {
+        cout << "Please declare the number of guests: " << endl;
+        cin >> guests;
+        if (guests > 0) {
+            valid_int = true;
+        }
+        else {
+            cout << "Please enter an integer between 1 and 8.";
+        }
+    }
+    return guests;
+}
+
