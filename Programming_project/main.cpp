f5c962cb92e96172be47b527fd3c172a59774c19
abigtent <100847889+abigtent@users.noreply.github.com>
2022-09-26 13:58:37 +0200

    Initial Commit

---
+++ Programming_project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Programming_project
+//
+//  Created by Henrik Reimers on 26/09/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
